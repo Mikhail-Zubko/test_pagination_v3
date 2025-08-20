70258bf0afb43cac74689d5abe8abb1ef8c4d583
Mikhail <99561817+Mikhail-Zubko@users.noreply.github.com>
2025-08-20 14:14:53 +0300

    update_files

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,10 +1,26 @@
 class Pagination {
-    constructor({ currentPage = 1, totalPages = 1, containerSelector = 'body' }) {
+    constructor({ 
+        currentPage = 1, 
+        totalPages = 1,
+        containerSelector = 'body' 
+        })  {
         this.currentPage = currentPage
         this.totalPages = totalPages
         this container = document.querySelector(containerSelector)
 
     }
+
+    render() {
+        let itemsTemplate
+        for(let i = 1; i <= this.totalPages; i += 1)
+            const template = `
+        <li class="page-item ${i === this.currentPage ? 'active' : ''}">
+            <a class="page-link" href="#">${i}</a>
+        </li>
+        `
+        itemsTemplate += template
+    }
+    console.log(itemsTemplate);
 }
 
 export default Pagination
---
+++ src/js/tasksService.js
@@ -0,0 +1,20 @@
+import { tasksMock } from './tasks'
+import { APP_TASKS_LS_KEY } from './constants'
+import { axios } from './axios'
+
+export function getTasks() {
+    return axios.get('/todos').then(res => res.data)
+}
+
+export function addNewTask(payload) {
+    const newTask = {
+        title: payload.title,
+        completed: false
+    }
+    return axios.post('/todos', newTask)
+}
+
+export function removeTask(id) {
+    return axios.delete(`/todos/${id}`)
+}
+
